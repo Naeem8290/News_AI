2f241575f703f78b9d5dc8be4466d9dc49f218e9
Naeem <naeemjaipur@gmail.com>
2025-02-14 18:18:59 +0530

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,38 @@
 import React from 'react'
-import { motion } from "motion/react"
+import Navbar from './components/Navbar'
+import '@mantine/core/styles.css';
+import { Routes , Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Register from './pages/Register'
 
 const App = () => {
   return (
     <div>
-      <motion.h2 animate={{rotate:360}} className='text-3xl text-red-500'>Hello World</motion.h2>
+      <Navbar/>
+      <Routes>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
+      </Routes>
     </div>
   )
 }
 
 export default App
+
+
+
+
+
+
+// import React from 'react'
+// import { motion } from "motion/react"
+
+// const App = () => {
+//   return (
+//     <div>
+//       <motion.h2 animate={{ rotate: 360 }} className='text-3xl text-red-500'>Hello World</motion.h2>
+//     </div>
+//   )
+// }
+
+// export default App
