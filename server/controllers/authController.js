12d3779894bf87e8ec1af738caa4240da0db72e0
Naeem8290 <naeemjaipur@gmail.com>
2025-06-16 13:14:16 +0530

    AWS

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -31,7 +31,7 @@ export const login = async (req, res) => {
     res.cookie('token', token, {
       httpOnly: true,
       sameSite : 'none' ,
-      secure : true,
+      secure : false,
       maxAge: 15 * 24 * 60 * 60 * 1000,
     });
     
